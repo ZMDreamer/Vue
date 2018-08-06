18c71aea139aa6fd004ecf1087aa52ea0b9e797a
ZMDreamer <1101370783@qq.com>
2018-08-06 15:39:42 +0800

    webpack-css,webpack-less&-scss&-font

--- webpack/webpack-dev-server/src/2.js
+++ webpack/webpack-dev-server/src/2.js
@@ -1,2 +1,5 @@
 const add = require('./1');
+require('../css/index.css');
+require('../css/less.less');
+require('../css/scss.scss')
 document.write(add(1,2));
--- webpack/webpack-dev-server/webpack.config.js
+++ webpack/webpack-dev-server/webpack.config.js
@@ -1,11 +1,50 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/2.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/dist',
-        filename: 'bundle.js'
-
-    }
+  entry: './src/2.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // 配置的是用来解析.css文件的loader(style-loader和css-loader)
+      {
+        // 1.0 用正则匹配当前访问的文件的后缀名是  .css
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'] //webpack底层调用这些包的顺序是从右到左
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'less-loader'
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif)/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            // limit表示如果图片大于50000byte，就以路径形式展示，小于的话就用base64格式展示
+            limit: 50000
+          }
+        }]
+      }
+    ]
+  }
 }
